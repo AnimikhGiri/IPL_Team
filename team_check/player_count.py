e39f7881d0cd05692b68305b0e0db7b1b079386c
AnimikhGiri <ani97utd@gmail.com>
2022-07-15 00:53:32 +0530

    adding comments

--- team_check/player_count.py
+++ team_check/player_count.py
@@ -1,3 +1,4 @@
+# function to count foreign players
 def count_foreign_players(team_data):
     foreign_count = 0
     for i in team_data['player']:
@@ -6,10 +7,10 @@ def count_foreign_players(team_data):
     return foreign_count
 
 
+# function to count wicketkeeper
 def count_wicketkeeper(team_data):
     wk_count = 0
     for i in team_data['player']:
         if i['role'] == 'Wicket-keeper':
             wk_count += 1
     return wk_count
-
