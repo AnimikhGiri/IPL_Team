63b05c389a66199e197e26dc8f1ec57e2904bbc3
AnimikhGiri <ani97utd@gmail.com>
2022-07-15 00:18:11 +0530

    Second commit

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,29 +1,22 @@
 
 import pytest
 from test_json.team_rcb_json import *
+from team_check.player_count import *
 
 
 @pytest.fixture()
 def load_team_data():
-    return load_json_data()
+    team = load_json_data()
+    return team
 
 
 def test_foreign_player_count(load_team_data):
-    team = load_team_data
-    foreign_sum = 0
-    for i in team['player']:
-        if i['country'] != 'India':
-            foreign_sum += 1
-    assert foreign_sum == 4, "Team cannot have more or less than 4 foreign players"
+    player_count = count_foreign_players(load_team_data)
+    assert player_count == 4, "Team cannot have more or less than 4 foreign players"
     print("Team has only 4 foreign players")
 
 
 def test_one_keeper(load_team_data):
-    team = load_team_data
-    count = 0
-
-    for i in team['player']:
-        if i['role'] == 'Wicket-keeper':
-            count += 1
-    assert count >= 1, "Team should have at least one WK"
+    player_count = count_wicketkeeper(load_team_data)
+    assert player_count >= 1, "Team should have at least one WK"
     print("Team has at least one wicket keeper")
