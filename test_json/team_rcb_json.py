158c751c96e47212cd6464e6b8bbe41d0ca67140
AnimikhGiri <ani97utd@gmail.com>
2022-07-15 00:56:32 +0530

    adding comments

--- test_json/team_rcb_json.py
+++ test_json/team_rcb_json.py
@@ -2,9 +2,9 @@ import json
 import urllib.request
 
 
+# loading json data from URL
 def load_json_data():
     url = "https://gist.githubusercontent.com/kumarpani/1e759f27ae302be92ad51ec09955e765/raw/184cef7125e6ef5a774e60de31479bb9b2884cb5/TeamRCB.json"
     response = urllib.request.urlopen(url)
     data_json = json.loads(response.read().decode())
     return data_json
-
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -12,11 +12,11 @@ def load_team_data():
 
 def test_foreign_player_count(load_team_data):
     player_count = count_foreign_players(load_team_data)
-    assert player_count == 4, "Team cannot have more or less than 4 foreign players"
+    assert player_count == 4, "Team cannot have more or less than 4 foreign players"    # validating foreign players count
     print("Team has only 4 foreign players")
 
 
 def test_one_keeper(load_team_data):
     player_count = count_wicketkeeper(load_team_data)
-    assert player_count >= 1, "Team should have at least one WK"
+    assert player_count >= 1, "Team should have at least one WK"    # validating wicketkeeper count
     print("Team has at least one wicket keeper")
